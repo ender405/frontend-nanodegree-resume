6be45342dcb67f6478aa2bf8b40c1ac8d231d7ed
ender405 <laslack@gmail.com>
2014-12-17 12:37:29 -0500

    Try to add contact and skills to resume

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,7 +1,22 @@
 
-var name = "Alex Slack"
-var formattedName = HTMLheaderName.replace("%data%", name)
-var role = "Research Analyst"
-var formattedRole = HTMLheaderRole.replace("%data%", role)
+var skills = ["Ruby", "Python", "Javascript"];
 
-$("#header").prepend(formattedName + formattedRole)
+var bio = {
+	"name" : "Alex Slack",
+	"role" : "Research Analyst",
+	"contact_info" : "email@gmail.com",
+	"picture_url" : "http://www.fcps.edu/islandcreekes/ecology/Amphibians/Bullfrog/bull2.jpg",
+	"welcome_message" : "Thanks for visiting my resume!",
+	"skills" : skills
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedbioPic = HTMLbioPic.replace("%data%", bio.picture_url);
+var formattedwelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcome_message);
+var formattedSkills = HTMLskills.replace("%data%", bio.skills);
+var formattedcontactInfo = HTMLcontactGeneric.replace("%data%", bio.contact_info);
+
+$("#header").prepend(formattedName + formattedRole);
+$("#main").prepend(formattedcontactInfo + formattedbioPic + formattedwelcomeMsg 
+	+ formattedSkills);
